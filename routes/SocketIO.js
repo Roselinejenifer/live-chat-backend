673c6ee05733af25fa6f24fa64ce02e333475ef0
Roseline Jenifer <91079271+Roselinejenifer@users.noreply.github.com>
2024-08-03 13:14:40 +0530

    Update SocketIO.js

--- routes/SocketIO.js
+++ routes/SocketIO.js
@@ -1,52 +1,3 @@
-// // import { Server } from 'socket.io';
-// const Server=require('socket.io')
-
-
-// let connection = null;
-
-// class SocketIO {
-//   constructor() {
-//     this.io = null;
-//   }
-
-//   connect(httpServer) {
-//     this.io = new Server(httpServer, {
-//       cors: {
-//         origin: "*",
-//       },
-//     });
-
-//     this.io.on("connection", (socket) => {
-//       console.log("User connected:", socket.id);
-
-//       socket.on("chat_message", (data) => {
-//         console.log("Message received:", data);
-//         this.io.emit("chat_message", data);
-//       });
-
-//       socket.on("disconnect", () => {
-//         console.log("User disconnected:", socket.id);
-//       });
-//     });
-//   }
-
-//   static init(server) {
-//     if (!connection) {
-//       connection = new SocketIO();
-//       connection.connect(server);
-//     }
-//   }
-
-//   static getSocketConnection() {
-//     if (connection) {
-//       return connection.io;
-//     }
-//     return null;
-//   }
-// }
-
-
-// module.exports = SocketIO;
 
 import { Server } from 'socket.io';
 
@@ -67,7 +18,6 @@ class SocketIO {
     this.io.on("connection", (socket) => {
       console.log("User connected:", socket.id);
 
-      // Handle incoming chat messages
       socket.on("chat_message", (data) => {
         console.log("Message received:", data);
         this.io.emit("chat_message", data);
